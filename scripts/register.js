9c6f88554d1ded403ff15287049cf0725b65a956
Jennifer Torres Orellana <115603025+Jayo94@users.noreply.github.com>
2023-01-12 01:33:01 -0800

    secondcommit
    Javascript is added

--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,6 @@
 const form = document.querySelector('.forms');
+
+
 const usernameInput = document.querySelector('#username');
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
@@ -7,6 +9,10 @@ const passwordInput = document.querySelector('#password');
 
 form.addEventListner('submit', (event) => {
     event.preventDefault();
-
+ // Prevent the form from refreshing the page,
+    // as it will do by default when the Submit event is triggered:
+    const formData = new FormData(this);
+    
     ValidateForm();
+    // Have the form Validate Users account info 
 })
