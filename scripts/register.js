65a53f53746de26bb43ba484f111df8d8f0c49f8
Jennifer Torres Orellana <115603025+Jayo94@users.noreply.github.com>
2023-01-12 02:22:46 -0800

    commit3
    first fetch for reg page

--- scripts/register.js
+++ scripts/register.js
@@ -1,18 +1,44 @@
-const form = document.querySelector('.forms');
+const form = document.querySelector('.forms2');
 
 
-const usernameInput = document.querySelector('#username');
-const emailInput = document.querySelector('#email');
-const passwordInput = document.querySelector('#password');
+//const usernameInput = document.querySelector('#username');
+//const emailInput = document.querySelector('#email');
+//const passwordInput = document.querySelector('#password');
 
 
 
 form.addEventListner('submit', (event) => {
     event.preventDefault();
- // Prevent the form from refreshing the page,
-    // as it will do by default when the Submit event is triggered:
+ // Prevent the form from submmiting 
+  
     const formData = new FormData(this);
+// getting the form data
+    fetch('https://microbloglite.herokuapp.com',{
+        method: 'post',
+        body: formData
+
+}) .then(function (response) {
+
+    return response.text();
+
+}).then(function(text) {
     
-    ValidateForm();
+    console.log(text);
+}).catch(function(error) {
+
+})
+
+})
+
+
+.then((data) => {
+    // Save the token in local storage
+    localStorage.setItem('token', data.token);
+    // redirect the user to the login page
+    window.location.href = '/login';
+
+
+
+    //ValidateForm();
     // Have the form Validate Users account info 
 })
