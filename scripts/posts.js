a06877f8c3e4bb564aff99635e32bf0d0e099b6b
Alberto Filho <108553756+AlbertoF13@users.noreply.github.com>
2023-01-17 10:39:15 -0800

    I commented all the classes because struggle

--- scripts/posts.js
+++ scripts/posts.js
@@ -1,58 +1,58 @@
 /* Posts Page JavaScript */
 "use strict";
-// callApi2()
+callApi2()
 
-class Posts {
-    constructor() {
-        this.posts = []
-    }
+// class Posts {
+//     constructor() {
+//         this.posts = []
+//     }
 
-    getPosts(limit , offset) {
-        const { token } = getLoginData();
-        fetch(`https://microbloglite.herokuapp.com/api/posts?limit=${limit}&offset=${offset}`, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`
-        }
-        })
-        .then(response => response.json())
-        .then(result => result.forEach(post => this.posts.push(new Post(post._id , post.text , post.username , post.createdAt , post.likes)
-        )
-        )
-        )
-        .catch(error => console.log("error", error));
-    }
+//     getPosts(limit , offset) {
+//         const { token } = getLoginData();
+//         fetch(`https://microbloglite.herokuapp.com/api/posts?limit=${limit}&offset=${offset}`, {
+//             method: "GET",
+//             headers: {
+//                 Authorization: `Bearer ${token}`
+//         }
+//         })
+//         .then(response => response.json())
+//         .then(result => result.forEach(post => this.posts.push(new Post(post._id , post.text , post.username , post.createdAt , post.likes)
+//         )
+//         )
+//         )
+//         .catch(error => console.log("error", error));
+//     }
 
-    displayPosts(mypost) {
-        // document.getElementById("post").innerHTML = this.posts.map(post => postTemplate(post)).join(" ")
+//     displayPosts(mypost) {
+//         // document.getElementById("post").innerHTML = this.posts.map(post => postTemplate(post)).join(" ")
 
-        console.log(this.posts.map(post => console.log(post)));
-    }
-}
+//         console.log(this.posts.map(post => console.log(post)));
+//     }
+// }
 
-class Post {
+// class Post {
 
-    constructor(id , text , username , createdAt , likes) {
-        this.id = id
-        this.text = text
-        this.username = username
-        this.createdAt =createdAt
-        this.likes = likes
-    }
+//     constructor(id , text , username , createdAt , likes) {
+//         this.id = id
+//         this.text = text
+//         this.username = username
+//         this.createdAt =createdAt
+//         this.likes = likes
+//     }
 
-    like() {
-        //todo
-    }
+//     like() {
+//         //todo
+//     }
 
-    unlike() {
-        //todo
-    }
+//     unlike() {
+//         //todo
+//     }
 
-    post() {
-        //todo
-    }
+//     post() {
+//         //todo
+//     }
 
-}
+// }
 
 
 function postTemplate(post) {
@@ -90,10 +90,10 @@ function callApi2() {
     .catch(error => console.log("error", error));
 }
 
-let allposts = new Posts()
-allposts.getPosts(1000 , 0)
-console.log(allposts.posts)
-allposts.displayPosts()
+// let allposts = new Posts()
+// allposts.getPosts(1000 , 0)
+// console.log(allposts.posts)
+// allposts.displayPosts()
 
 
 // post() {
