b3657511cdc0b2e1dbf15e2248837e0587fcce21
Alberto Filho <108553756+AlbertoF13@users.noreply.github.com>
2023-01-18 15:07:16 -0800

    trying to fix the like button

--- scripts/posts.js
+++ scripts/posts.js
@@ -16,7 +16,7 @@ class Posts {
         }
         })
         .then(response => response.json())
-        .then(result => {result.forEach(post => this.posts.push(new Post(post._id , post.text , post.username , post.createdAt , post.likes)))})
+        .then(result => {result.forEach(post => this.posts.push(new Post( post._id , post.text )))})
         .then(result => this.displayPosts())
         .catch(error => console.log("error", error));
     }
@@ -28,16 +28,33 @@ class Posts {
 
 class Post {
 
-    constructor(id , text , username , createdAt , likes) {
+    constructor( id , text ) {
         this.id = id
         this.text = text
-        this.username = username
-        this.createdAt =createdAt
-        this.likes = likes
+        // console.log(this.id)
     }
 
     like() {
-        return console.log(this.id)
+        const { token } = getLoginData();
+        var myHeaders = new Headers();
+        myHeaders.append("Authorization", `Bearer ${token}`);
+        myHeaders.append("Content-Type", "application/json");
+        console.log(myHeaders)
+        var raw = JSON.stringify({
+            "postId": `${this.id}`
+        });
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+
+        fetch("https://microbloglite.herokuapp.com/api/likes", requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(this.id))
+        .catch(error => console.log('error', error));
     }
 
     unlike() {
@@ -52,12 +69,10 @@ class Post {
     post() {
         //todo
     }
-
 }
 
-
 function postTemplate(post) {
-    return `
+    const template = `
     <div class="card">
         <div class="card-content">
             <div class="media-content">
@@ -69,23 +84,56 @@ function postTemplate(post) {
             <div class="content">
             <p>${post.text}</p>
             <br>
-            <!-- create a button here -->
+            <button id="${post.id}">Like</button>
             </br>
             <time datetime="2016-1-1">${post.createdAt}</time>
             </div>
         </div>
     </div>
     `
+    const currentpost = document.getElementById(post.id)
+    // .addEventListener("click" , function(){
+    //     console.log(post.id)
+    console.log(currentpost)
+    // })
+    return template
 }
 
 let allposts = new Posts()
 allposts.getPosts(1000 , 0)
 console.log(allposts.likes)
 
+// let singlepost = new Post()
+// singlepost.like()
+// console.log(singlepost)
 
 
 
 
+// function liking(){
+//         const { token } = getLoginData();
+//         var myHeaders = new Headers();
+// myHeaders.append("Authorization", `Bearer ${token}`);
+// myHeaders.append("Content-Type", "application/json");
+
+// var raw = JSON.stringify({
+//   "postId": "63bf08714848e691b6ddfd03"
+// });
+
+// var requestOptions = {
+//   method: 'POST',
+//   headers: myHeaders,
+//   body: raw,
+//   redirect: 'follow'
+// };
+
+// fetch("https://microbloglite.herokuapp.com/api/likes", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error)); 
+// }
+
+// liking()
 
 // function callApi2() {
 //     const limit = 1000;
